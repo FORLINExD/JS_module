d021277db925e45549069c8d14980334fd18b907
FORLINExD <forlinemv@gmail.com>
2021-11-11 18:24:04 +0200

    add errors in cw 1

---
+++ Classwork_1/errors/script.js
@@ -0,0 +1,33 @@
+// Ваше завдання, зробити так, щоб файл відкривався без помилок .
+
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// const x = 'constant value';
+// {
+//     console.log(a);
+//     x = 'new value';
+// }
+// let b = 'new value';
+//
+// let result = 100 + y;
+//
+// console(result);
+
+
+
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+{
+    console.log(a, b, c, d, x);
+    a = 'new value';
+}
+let y = ' new value';
+
+let result = 100 + y;
+
+console.log(result);
